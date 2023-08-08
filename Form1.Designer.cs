8b9affb0fa20fa6d6c290ff9befabed7e6203321
EloySalazar <102320132+EloySalazar@users.noreply.github.com>
2023-08-07 22:31:53 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace reconocimiento_de_voz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.texto = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // texto
+            // 
+            this.texto.Enabled = false;
+            this.texto.Location = new System.Drawing.Point(30, 57);
+            this.texto.Multiline = true;
+            this.texto.Name = "texto";
+            this.texto.Size = new System.Drawing.Size(231, 100);
+            this.texto.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.texto);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox texto;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.Globalization;
+using System.Runtime.InteropServices;
+
+namespace reconocimiento_de_voz
+{
+
+    public partial class Form1 : Form
+    {
+        SpeechSynthesizer _synthesizer = new SpeechSynthesizer();
+        //  SpeechRecognitionEngine _Recognition = new SpeechRecognitionEngine(new CultureInfo("es-VE"));
+        SpeechRecognitionEngine _Recognition = new SpeechRecognitionEngine();
+        public Form1()
+        {
+
+            InitializeComponent();
+
+                _Recognition.SetInputToDefaultAudioDevice();
+                _Recognition.LoadGrammar(new DictationGrammar());
+                _Recognition.SpeechRecognized += _Recognition_SpeechRecognized;
+                _Recognition.RecognizeAsync(RecognizeMode.Multiple);
+         
+        }
+
+        private void _Recognition_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            RecognitionResult a = e.Result;
+            texto.Text = a.Text;
+            _synthesizer.SelectVoice("eSpeak-es");
+            _synthesizer.SpeakAsync(texto.Text);
+           
+        }
+    }
+}
